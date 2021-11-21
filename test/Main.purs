ce069164ff36294eb4613c5d0b8700d629819629
hbybyyang <2451759073@qq.com>
2021-11-21 19:51:13 +0800

    debug

--- test/Main.purs
+++ test/Main.purs
@@ -3,7 +3,7 @@ module Test.Main where
 import Prelude
 import Effect (Effect)
 import Effect.Class.Console (log)
-import Hby.Prmoise (delay, liftEff, runTask_)
+import Hby.Task (delay, liftEff, runTask_)
 
 main :: Effect Unit
 main =
