43ef50eaaacd26b07ab20b337291dc8bdf4ac1bf
hbybyyang <2451759073@qq.com>
2022-02-03 13:48:51 +0800

    实现Semigroup

--- src/Hby/Task.purs
+++ src/Hby/Task.purs
@@ -5,6 +5,7 @@ import Effect (Effect)
 
 foreign import data Task :: Type -> Type
 
+-------------------------
 foreign import _map :: forall a b. (a -> b) -> Task a -> Task b
 
 foreign import _apply :: forall a b. Task (a -> b) -> Task a -> Task b
@@ -13,12 +14,16 @@ foreign import _pure :: forall a. a -> Task a
 
 foreign import _bind :: forall a b. Task a -> (a -> Task b) -> Task b
 
+-------------------------
 foreign import runTask :: forall a. Task a -> (a -> Unit) -> Effect Unit
 
 foreign import runTask_ :: forall a. Task a -> Effect Unit
 
 foreign import liftEff :: forall a b. (a -> Effect b) -> (a -> Task b)
 
+foreign import delay :: Int -> Task Unit
+
+-------------------------
 instance mapTask :: Functor Task where
   map :: forall a b. (a -> b) -> Task a -> Task b
   map = _map
@@ -35,4 +40,9 @@ instance bindTask :: Bind Task where
   bind :: forall a b. Task a -> (a -> Task b) -> Task b
   bind = _bind
 
-foreign import delay :: Int -> Task Unit
+instance appendTask :: Semigroup a => Semigroup (Task a) where
+  append :: Task a -> Task a -> Task a
+  append a b = do
+    aa <- a
+    bb <- b
+    pure $ aa <> bb
